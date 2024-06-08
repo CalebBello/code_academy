6d2e49d72344fa2b305047a09bb914cfee2a56d8
Caleb Bello <caleb.m.bello@gmail.com>
2024-06-08 20:03:27 +0200

    Code added
    Console art

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
 ﻿
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {//this prints some dots
+      Console.WriteLine("  ;;;;;");    
+      Console.WriteLine("  (o o)"); 
+      Console.WriteLine(@" /| O | \");
+      Console.WriteLine(@"/ |   |  \");
+      Console.WriteLine("  '~~~' ");
+      Console.WriteLine("  || ||");
+      Console.WriteLine(" (O   O)");
+      Console.WriteLine(" Pepito");
+
+      Console.ReadLine();
+     }
+  }
